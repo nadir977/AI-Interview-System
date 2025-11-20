c3fd89e456ed9ab942ff51fd00c9d9683c078d19
nadir977 <nadirali4622@gmail.com>
2025-11-20 10:56:31 +0500

    fix: next.config cleaned for Next.js 16

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   // images: {
   //   remotePatterns: [
   //     {
@@ -11,9 +10,7 @@ const nextConfig: NextConfig = {
   //     },
   //   ],
   // },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+
   typescript: {
     ignoreBuildErrors: true,
   },
